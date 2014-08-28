089e9d41b3e0753c9460c6f64f57547764cb529f
Sivachandran Pushpanathan <sivachandran.pushpanathan@cgi.com>
2014-08-28 10:35:56 +0000

    reverse merged #24413
    git-svn-id: file:///mnt/ramdisk/d4-svn/trunk@24470 484df14f-75d2-4b3f-8ba0-490e05069a01

--- agiletools/htdocs/js/taskboard.js
+++ agiletools/htdocs/js/taskboard.js
@@ -817,9 +817,7 @@
           ticket: ticket,
           group: newGroup
         });
-      // Bind events from userfield js 
-      $('.user-field').userField();
-      
+
       $("select", this.$optDialog).select2({
         width: "off",
         dropdownCssClass: "ui-dialog",
